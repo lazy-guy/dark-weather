6aefa59cbd9fe592b66772ac98e26d209d0bf829
Madhur_Saini <devmadhursaini@gmail.com>
2019-04-29 19:46:14 +0530

    Updated Search

--- index.js
+++ index.js
@@ -22,28 +22,72 @@ if (localStorage.getItem("city")) {
     if (navigator.onLine) {
         req(city);
     }
-}else{
+} else {
     search();
 }
+
+var tempre;
+var citylist;
+async function fetchjson() {
+    const cityList = await fetch("city.list.min.json");
+    citylist = await cityList.json();
+}
+
+function cities() {
+    if (citylist) {
+        if(document.getElementById("cityquery").value.length == 0){
+            document.getElementById("finalists").innerHTML = "";
+            return;
+        }
+        var str = document.getElementById("cityquery").value;
+        tempre = new RegExp(`^${str}`, 'gi')
+        var finale = citylist.filter(isfinalist);
+        var cityhtml = " ";
+        var ii;
+        if (finale.length > 50) {
+            ii = 50
+        } else {
+            ii = finale.length;
+        }
+
+        for (var i = 0; i < ii; i++) {
+            var obj = finale[i];
+            cityhtml += `<div class="card" onclick="aftersearch(${obj.id})"> ${obj.name}, ${obj.country.toUpperCase()}<br />Lat = ${obj.coord.lat} , Long = ${obj.coord.lon}</div>`
+        }
+        if(cityhtml !== " "){
+        document.getElementById("finalists").innerHTML = cityhtml;
+        }else{
+            document.getElementById("finalists").innerHTML = "<h4>No city found!</h4>";
+        }
+    }
+}
+
+function isfinalist(src) {
+    if (tempre.test(src.name)) {
+        return src.name;
+    }
+}
+
 
 function req(place) {
     date = new Date();
     utcTime = parseInt(date.getTime() / 1000);
-    if (typeof place === "string" && typeof last !== "undefined" && last.name.toUpperCase() == place.toUpperCase() && utcTime - last.dt < 1200) {
+    if (typeof last !== "undefined" && last.id == place && utcTime - last.dt < 1200) {
         init(last);
     } else {
-        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${place}&units=${unit}&APPID=${apikey}`).then(function (result) {
+        fetch(`https://api.openweathermap.org/data/2.5/weather?id=${place}&units=${unit}&APPID=${apikey}`).then(function (result) {
             return result.json()
         }).then(function (res) {
             console.log(res);
             init(res);
         });
     }
-    if (typeof lastforecast !== "undefined" && lastforecast.city.name.toUpperCase() == place.toUpperCase() && utcTime - last.dt < 3600) {
-        console.log(lastforecast);
-        forecast(lastforecast);
+    if (typeof lastforecast !== "undefined" && lastforecast.cod !== "404") {
+        if (lastforecast.city.id == place && utcTime - last.dt < 3600) {
+            forecast(lastforecast);
+        }
     } else {
-        fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${place}&units=${unit}&APPID=${apikey}`).then(function (result) {
+        fetch(`https://api.openweathermap.org/data/2.5/forecast?id=${place}&units=${unit}&APPID=${apikey}`).then(function (result) {
             return result.json()
         }).then(function (fore) {
             console.log(fore);
@@ -58,60 +102,66 @@ function forecast(fore) {
     var tempdate;
     if (fore.cod === "404") {
         return;
-    }else
-    for (var i = 0; i < 5; i++) {
-        tempdate = new Date(fore.list[i].dt*1000);
-        if(tempdate.getDate() === lastTime.getDate()){
-        todaytemp.push(fore.list[i].main.temp);
+    } else
+        for (var i = 0; i < 5; i++) {
+            tempdate = new Date(fore.list[i].dt * 1000);
+            if (tempdate.getDate() === lastTime.getDate()) {
+                todaytemp.push(fore.list[i].main.temp);
+            }
         }
-    }
     maxtemp = Math.max.apply(Math, todaytemp);
     mintemp = Math.min.apply(Math, todaytemp);
     document.querySelector("#minmaxtext").innerText = "Max/Min";
     document.querySelector("#minmax").innerHTML = parseInt(maxtemp) + "&deg;C/" + parseInt(mintemp) + "&deg;C";
     var temphtml = "";
-    tempdate = new Date(fore.list[0].dt*1000);
+    tempdate = new Date(fore.list[0].dt * 1000);
     var basedate = tempdate.getDate();
     var basenum = 1;
     var foredate;
     var foremonth;
     for (let i = 0; i < fore.list.length; i++) {
-        tempdate = new Date(fore.list[i].dt*1000);
-        if(tempdate.getDate() == basedate){
+        tempdate = new Date(fore.list[i].dt * 1000);
+        if (tempdate.getDate() == basedate) {
             temphtml += `<div class="hours">
         <div class="wtitle">${fore.list[i].weather[0].main}&nbsp;${fore.list[i].main.temp.toFixed(0)}&deg;</div>
         <i class="icon wi wi-owm-${fore.list[i].weather[0].id}"></i>
         <div class="time">${tempdate.getHours()}:00</div>
         </div>`;
-        }else{
+        } else {
             basedate = tempdate.getDate();
-            tempdate = new Date(fore.list[i - 1].dt*1000);
+            tempdate = new Date(fore.list[i - 1].dt * 1000);
             foredate = tempdate.getDate();
             foremonth = tempdate.getMonth() + 1;
-            if(typeof document.querySelector("#d"+basenum) === "object"){
-            document.querySelector("#d"+basenum).innerText = foremonth + "/" + foredate;
-            document.querySelector("#day"+basenum).innerHTML = temphtml;
-            temphtml = "";
-            basenum++;
+            if (typeof document.querySelector("#d" + basenum) === "object") {
+                document.querySelector("#d" + basenum).innerText = foremonth + "/" + foredate;
+                document.querySelector("#day" + basenum).innerHTML = temphtml;
+                temphtml = "";
+                basenum++;
             }
         }
-        if(i === fore.list.length - 1){
+        if (i === fore.list.length - 1) {
             basedate = tempdate.getDate();
-            tempdate = new Date(fore.list[i - 1].dt*1000);
+            tempdate = new Date(fore.list[i - 1].dt * 1000);
             foredate = tempdate.getDate();
             foremonth = tempdate.getMonth() + 1;
-            if(document.querySelector("#d"+basenum)){
-            document.querySelector("#d"+basenum).innerText = foremonth + "/" + foredate;
-            document.querySelector("#day"+basenum).innerHTML = temphtml;
-            temphtml = "";
-            basenum++;
+            if (document.querySelector("#d" + basenum)) {
+                document.querySelector("#d" + basenum).innerText = foremonth + "/" + foredate;
+                document.querySelector("#day" + basenum).innerHTML = temphtml;
+                temphtml = "";
+                basenum++;
             }
         }
     }
 }
 
 function search() {
-    req(prompt("Enter City Name"));
+    location.hash = "#citylist";
+}
+
+function aftersearch(id){
+    location.hash = "#";
+    setTimeout(function(){document.getElementById("finalists").innerHTML = "";document.getElementById("cityquery").value = ""}, 1000)
+    req(id);
 }
 
 function init(res) {
@@ -119,7 +169,7 @@ function init(res) {
         alert("City not found!");
         return;
     } else {
-        city = res.name;
+        city = res.id;
         localStorage.setItem("city", city);
         document.querySelector("#extras").style.display = "block";
         document.querySelector("#temp").innerHTML = res.main.temp.toFixed(0) + "&deg;C";
@@ -139,51 +189,52 @@ function init(res) {
             document.querySelector("#wind").innerHTML = res.wind.speed + ' m/s&nbsp;&nbsp;<i id="windimg"></i>';
             var winddeg = parseInt(res.wind.deg);
             var direction;
-            if(winddeg >=0 && winddeg <= 22){
+            if (winddeg >= 0 && winddeg <= 22) {
                 direction = "n";
             }
-            if(winddeg >= 23 && winddeg <= 44){
+            if (winddeg >= 23 && winddeg <= 44) {
                 direction = "nne";
             }
-            if(winddeg >= 45 && winddeg <= 67){
+            if (winddeg >= 45 && winddeg <= 67) {
                 direction = "ne";
             }
-            if(winddeg >=68 && winddeg <= 89){
+            if (winddeg >= 68 && winddeg <= 89) {
                 direction = "ene";
             }
-            if(winddeg >=90 && winddeg <= 112){
+            if (winddeg >= 90 && winddeg <= 112) {
                 direction = "e";
             }
-            if(winddeg >=113 && winddeg <= 134){
+            if (winddeg >= 113 && winddeg <= 134) {
                 direction = "ese";
             }
-            if(winddeg >=135 && winddeg <= 157){
+            if (winddeg >= 135 && winddeg <= 157) {
                 direction = "se";
-            }if(winddeg >=158 && winddeg <= 179){
+            }
+            if (winddeg >= 158 && winddeg <= 179) {
                 direction = "sse";
             }
-            if(winddeg >=180 && winddeg <= 202){
+            if (winddeg >= 180 && winddeg <= 202) {
                 direction = "s";
             }
-            if(winddeg >=203 && winddeg <= 224){
+            if (winddeg >= 203 && winddeg <= 224) {
                 direction = "ssw";
             }
-            if(winddeg >=225 && winddeg <= 247){
+            if (winddeg >= 225 && winddeg <= 247) {
                 direction = "sw";
             }
-            if(winddeg >=248 && winddeg <= 269){
+            if (winddeg >= 248 && winddeg <= 269) {
                 direction = "wsw";
             }
-            if(winddeg >=270 && winddeg <= 292){
+            if (winddeg >= 270 && winddeg <= 292) {
                 direction = "w";
             }
-            if(winddeg >=293 && winddeg <= 312){
+            if (winddeg >= 293 && winddeg <= 312) {
                 direction = "wnw";
             }
-            if(winddeg >=313 && winddeg <= 335){
+            if (winddeg >= 313 && winddeg <= 335) {
                 direction = "nw";
             }
-            if(winddeg >=336 && winddeg <= 359){
+            if (winddeg >= 336 && winddeg <= 359) {
                 direction = "nnw";
             }
             document.querySelector("#windimg").className = "wi wi-wind wi-towards-" + direction + "-deg";
@@ -223,7 +274,8 @@ function init(res) {
         console.log();
         last = res;
         localStorage.setItem("last", JSON.stringify(last));
-        lastTime = new Date(last.dt*1000);
+        lastTime = new Date(last.dt * 1000);
     }
 }
 
+fetchjson();
--- sw.js
+++ sw.js
@@ -1,24 +1,35 @@
-self.addEventListener('install', function(e) {
-    e.waitUntil(
-      caches.open('dark-weather').then(function(cache) {
-        return cache.addAll([
-          '/dark-weather',
-          '/dark-weather/index.html',
-          '/dark-weather/index.js',
-          '/dark-weather/css/index.css',
-          '/dark-weather/css/weather-icons-wind.min.css',
-          '/dark-weather/css/weather-icons.min.css',
-          '/dark-weather/font/weathericons-regular-webfont.woff2'
-        ]);
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open('dark-weather').then(function (cache) {
+      return cache.addAll([
+        '/dark-weather',
+        '/dark-weather/index.html',
+        '/dark-weather/index.js',
+        '/dark-weather/css/index.css',
+        '/dark-weather/css/weather-icons-wind.min.css',
+        '/dark-weather/css/weather-icons.min.css',
+        '/dark-weather/font/weathericons-regular-webfont.woff2'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(request)
+    .then(response => {
+      if (response.ok)
+        addToCache(pagesCacheName, request, response.clone());
+      return response;
+    })
+    .catch(() => {
+      return caches.match(request).then(response => {
+        return response;
       })
-    );
-   });
-   
-   self.addEventListener('fetch', function(e) {
-     console.log(e.request.url);
-     e.respondWith(
-       caches.match(e.request).then(function(response) {
-         return response || fetch(e.request);
-       })
-     );
-   });
+    })
+  );
+});
+
+self.addEventListener("onupdatefound", function () {
+  registration.update();
+})
